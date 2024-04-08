15693ca449b28b3a3856a99b16150acfa9cbdc68
Aditya <holaaditya123@gmail.com>
2024-04-08 23:34:10 +0530

    Fixing the styles of HomeCard component

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,13 +1,15 @@
-import { cn } from "@/lib/utils";
+"use client";
+
 import Image from "next/image";
-import React from "react";
+
+import { cn } from "@/lib/utils";
 
 interface HomeCardProps {
-  className: string;
+  className?: string;
   img: string;
   title: string;
   description: string;
-  handleClick: () => void;
+  handleClick?: () => void;
 }
 
 const HomeCard = ({
@@ -18,13 +20,11 @@ const HomeCard = ({
   handleClick,
 }: HomeCardProps) => {
   return (
-    <div
-      className={
-        (cn(
-          "px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer"
-        ),
-        className)
-      }
+    <section
+      className={cn(
+        "px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer",
+        className
+      )}
       onClick={handleClick}
     >
       <div className="flex-center glassmorphism size-12 rounded-[10px]">
@@ -35,7 +35,7 @@ const HomeCard = ({
         <h1 className="text-2xl font-bold">{title}</h1>
         <p className="text-lg font-normal">{description}</p>
       </div>
-    </div>
+    </section>
   );
 };
 
