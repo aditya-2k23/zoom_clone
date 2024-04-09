103d2f442474c0aa10e1554e07c32e20e648faf3
Aditya <holaaditya123@gmail.com>
2024-04-09 20:07:03 +0530

    Update MeetingTypeList.tsx to include StreamVideoProvider and MeetingModal component

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import HomeCard from "./HomeCard";
+import MeetingModal from "./MeetingModal";
 import { useRouter } from "next/navigation";
 
 const MeetingTypeList = () => {
@@ -10,13 +11,15 @@ const MeetingTypeList = () => {
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
 
+  const createMeeting = () => {};
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
@@ -40,8 +43,15 @@ const MeetingTypeList = () => {
         handleClick={() => setMeetingState("isJoiningMeeting")}
         className="bg-yellow-1"
       />
-    
-      <MeetingModal /> {/*Import this file from components*/}
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
     </section>
   );
 };
