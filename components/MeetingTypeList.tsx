36652c57ff08a2845ebc1b3094cbc371ccd78864
Aditya <holaaditya123@gmail.com>
2024-04-09 20:47:44 +0530

    Added the toast (temporary popup with a message of "meeting created")

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -6,6 +6,7 @@ import MeetingModal from "./MeetingModal";
 import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "@/components/ui/use-toast";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -23,10 +24,19 @@ const MeetingTypeList = () => {
 
   const [callDetails, setCallDetails] = useState<Call>();
 
+  const { toast } = useToast();
+
   const createMeeting = async () => {
     if (!client || !user) return;
 
     try {
+      if (!values.dateTime) {
+        toast({
+          title: "Please select a date and time for the meeting.",
+        });
+        return;
+      }
+
       const id = crypto.randomUUID();
       const call = client.call("default", id);
 
@@ -50,8 +60,13 @@ const MeetingTypeList = () => {
       if (!values.description) {
         router.push(`/meeting/${call.id}`);
       }
+
+      toast({ title: "Meeting Created" });
     } catch (error) {
       console.log(error);
+      toast({
+        title: "Failed to create meeting",
+      });
     }
   };
 
