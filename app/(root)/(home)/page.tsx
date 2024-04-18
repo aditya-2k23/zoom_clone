77a3d6794af09f167ac19ec9a0dfff2ce9552930
Aditya <holaaditya123@gmail.com>
2024-04-18 18:22:00 +0530

    Update upcoming meeting display in page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import MeetingTypeList from "@/components/MeetingTypeList";
+import { useGetCalls } from "@/hooks/useGetCalls";
 
 const Home = () => {
   const now = new Date();
@@ -15,12 +18,18 @@ const Home = () => {
     weekday: "short",
   });
 
+  const { upcomingCalls } = useGetCalls();
+  const upcomingMeetingTime = upcomingCalls.map((call) =>
+    call.state.startsAt?.toLocaleTimeString("INDIA")
+  );
   return (
-    <section className="flex size-full flex-col gap-10 text-white">
-      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+    <section className="flex size-full flex-col gap-5 text-white">
+      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30PM
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+            {upcomingCalls
+              ? `Upcoming Meeting at: ${upcomingMeetingTime[0]}`
+              : "No upcoming meeting"}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
