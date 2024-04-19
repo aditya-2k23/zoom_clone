a9843515ed284c9c66ce609e76961e3d85d0ec61
Aditya <holaaditya123@gmail.com>
2024-04-19 11:07:12 +0530

    Fix upcoming meeting time display. Always return the 0th index

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -32,7 +32,7 @@ const Home = () => {
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
             {upcomingCalls
-              ? `Upcoming Meeting at: ${upcomingMeetingTime[1]}`
+              ? `Upcoming Meeting at: ${upcomingMeetingTime[0]}`
               : "No upcoming meeting"}
           </h2>
           <div className="flex flex-col gap-2">
